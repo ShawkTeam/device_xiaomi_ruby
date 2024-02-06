e8bf8fd0f35c3a2ffe35a2d47a371123e8c0f389
Giovanni Ricca <giovanniricca@proton.me>
2024-02-06 16:13:46 +0100

    ruby: Build goodix lib from source
    Change-Id: Ibaf1b9489b509318032c720c9c83e315cfe6ab3e

--- device.mk
+++ device.mk
@@ -140,6 +140,9 @@ PRODUCT_PACKAGES += \
     vendor.xiaomi.hardware.fingerprintextension@1.0.vendor \
     vendor.xiaomi.hardware.fx.tunnel@1.0.vendor
 
+PRODUCT_PACKAGES += \
+    libvendor.goodix.hardware.biometrics.fingerprint@2.1.vendor
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
